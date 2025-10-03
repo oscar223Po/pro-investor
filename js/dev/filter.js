33bd4df589896f452a985f128c338a084c13bab5
Oscar <oscaritoparapito225@mail.ru>
2025-10-03 13:15:41 +0600

    Updates

--- js/dev/filter.js
+++ js/dev/filter.js
@@ -1,1 +1,10 @@
-
+const links = document.querySelectorAll(".filter__link");
+if (links !== null) {
+  links.forEach((link) => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.querySelector(".filter__link--active")?.classList.remove("filter__link--active");
+      this.classList.add("filter__link--active");
+    });
+  });
+}
